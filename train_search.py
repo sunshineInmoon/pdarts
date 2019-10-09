e1b700a4af107751a6ec89409aa7a484a9a78741
SunshineInMoon <451413025@qq.com>
2019-10-09 16:54:21 +0800

    Update train_search.py

--- train_search.py
+++ train_search.py
@@ -100,42 +100,44 @@ def main():
     # build Network
     criterion = nn.CrossEntropyLoss()
     criterion = criterion.cuda()
-    switches = []
-    for i in range(14):
-        switches.append([True for j in range(len(PRIMITIVES))])
-    switches_normal = copy.deepcopy(switches)
-    switches_reduce = copy.deepcopy(switches)
+    switches = [] #switch开关，标记path中哪个op是开放状态
+    for i in range(14): # 一个Cell中有4个计算Node，2个输入Node，因此总的path为14
+        switches.append([True for j in range(len(PRIMITIVES))]) #每个path上有len(PRIMITIVES)个op，初试状态是全部都用
+    switches_normal = copy.deepcopy(switches) #normal cell 中op的开关状态
+    switches_reduce = copy.deepcopy(switches) #reduce cell 中op的开关状态
     # To be moved to args
     num_to_keep = [5, 3, 1]
     num_to_drop = [3, 2, 2]
-    if len(args.add_width) == 3:
+    if len(args.add_width) == 3: #默认参数为0
         add_width = args.add_width
     else:
         add_width = [0, 0, 0]
-    if len(args.add_layers) == 3:
+    if len(args.add_layers) == 3: #传进去两个args.add_layers
         add_layers = args.add_layers
     else:
         add_layers = [0, 6, 12]
-    if len(args.dropout_rate) ==3:
+    if len(args.dropout_rate) ==3: #传进去三个参数
         drop_rate = args.dropout_rate
     else:
         drop_rate = [0.0, 0.0, 0.0]
     eps_no_archs = [10, 10, 10]
-    for sp in range(len(num_to_keep)):
+    for sp in range(len(num_to_keep)): #训练分为3个阶段
+        # args.init_channels 默认16，即网络的head输出channels为16，通过设置args.add_width可以改变这个输出channels
+        # args.layers 默认5，3个normal cell + 2个reduce cell，第二阶段layer为11，第三阶段为17，这通过add_layers调节
         model = Network(args.init_channels + int(add_width[sp]), CIFAR_CLASSES, args.layers + int(add_layers[sp]), criterion, switches_normal=switches_normal, switches_reduce=switches_reduce, p=float(drop_rate[sp]))
         model = nn.DataParallel(model)
         model = model.cuda()
         logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
-        network_params = []
+        network_params = [] # 保存网络权重
         for k, v in model.named_parameters():
             if not (k.endswith('alphas_normal') or k.endswith('alphas_reduce')):
                 network_params.append(v)       
         optimizer = torch.optim.SGD(
-                network_params,
+                network_params, # 负责更新网络权重
                 args.learning_rate,
                 momentum=args.momentum,
                 weight_decay=args.weight_decay)
-        optimizer_a = torch.optim.Adam(model.module.arch_parameters(),
+        optimizer_a = torch.optim.Adam(model.module.arch_parameters(), # 负责更新网络架构参数
                     lr=args.arch_learning_rate, betas=(0.5, 0.999), weight_decay=args.arch_weight_decay)
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
                 optimizer, float(args.epochs), eta_min=args.learning_rate_min)
@@ -143,17 +145,17 @@ def main():
         epochs = args.epochs
         eps_no_arch = eps_no_archs[sp]
         scale_factor = 0.2
-        for epoch in range(epochs):
+        for epoch in range(epochs): # 训练epoch，默认25，每个阶段训练25个epoch
             scheduler.step()
             lr = scheduler.get_lr()[0]
             logging.info('Epoch: %d lr: %e', epoch, lr)
             epoch_start = time.time()
             # training
-            if epoch < eps_no_arch:
+            if epoch < eps_no_arch: #前eps_no_arch（10） 训练网络权重
                 model.module.p = float(drop_rate[sp]) * (epochs - epoch - 1) / epochs
                 model.module.update_p()
                 train_acc, train_obj = train(train_queue, valid_queue, model, network_params, criterion, optimizer, optimizer_a, lr, train_arch=False)
-            else:
+            else: # 训练网络架构参数
                 model.module.p = float(drop_rate[sp]) * np.exp(-(epoch - eps_no_arch) * scale_factor) 
                 model.module.update_p()                
                 train_acc, train_obj = train(train_queue, valid_queue, model, network_params, criterion, optimizer, optimizer_a, lr, train_arch=True)
@@ -161,30 +163,35 @@ def main():
             epoch_duration = time.time() - epoch_start
             logging.info('Epoch time: %ds', epoch_duration)
             # validation
-            if epochs - epoch < 5:
+            if epochs - epoch < 5: # epochs=25,即最后5个epoch，在验证集上验证下
                 valid_acc, valid_obj = infer(valid_queue, model, criterion)
                 logging.info('Valid_acc %f', valid_acc)
-        utils.save(model, os.path.join(args.save, 'weights.pt'))
-        print('------Dropping %d paths------' % num_to_drop[sp])
+        utils.save(model, os.path.join(args.save, 'weights.pt')) #一个阶段的训练结束后，保存下模型
+        # 一个阶段训练完后，丢弃一些Op
+        print('------Dropping %d paths------' % num_to_drop[sp]) # num_to_drop=[3,2,2]，共有8个候选op，依次丢弃3，2，2，最后剩下一个
         # Save switches info for s-c refinement. 
-        if sp == len(num_to_keep) - 1:
-            switches_normal_2 = copy.deepcopy(switches_normal)
+        if sp == len(num_to_keep) - 1: # num_to_keep=[5,3,1], 只有在sp=2时，即最后一个训练阶段，才执行下面的语句
+            switches_normal_2 = copy.deepcopy(switches_normal) #此时每条path中还有3个op，后面要删除掉2个，剩下最后一个
             switches_reduce_2 = copy.deepcopy(switches_reduce)
         # drop operations with low architecture weights
-        arch_param = model.module.arch_parameters()
-        normal_prob = F.softmax(arch_param[0], dim=sm_dim).data.cpu().numpy()        
-        for i in range(14):
-            idxs = []
-            for j in range(len(PRIMITIVES)):
-                if switches_normal[i][j]:
-                    idxs.append(j)
-            if sp == len(num_to_keep) - 1:
+        # 放弃拥有低概率的op
+        arch_param = model.module.arch_parameters() #获取结构参数
+        # 处理arch_normal
+        normal_prob = F.softmax(arch_param[0], dim=sm_dim).data.cpu().numpy() # 计算arch_normal的softmax       
+        for i in range(14): #一个Cell中共有14条path
+            idxs = [] #记录每条path上选择的op的索引
+            for j in range(len(PRIMITIVES)): # 遍历每条path上的op
+                if switches_normal[i][j]: #如果为True，即选择它
+                    idxs.append(j) #idxs中有3个元素
+            if sp == len(num_to_keep) - 1: # 最后一个训练阶段
                 # for the last stage, drop all Zero operations
-                drop = get_min_k_no_zero(normal_prob[i, :], idxs, num_to_drop[sp])
+                # 对于最后一个训练阶段，丢弃所有 Zero 操作
+                drop = get_min_k_no_zero(normal_prob[i, :], idxs, num_to_drop[sp]) #最后一个阶段num_to_drop[2]=1
             else:
                 drop = get_min_k(normal_prob[i, :], num_to_drop[sp])
             for idx in drop:
-                switches_normal[i][idxs[idx]] = False
+                switches_normal[i][idxs[idx]] = False #将概率最低的k个op关闭，注意此处更新了switches_normal
+        # 处理arch_reduce
         reduce_prob = F.softmax(arch_param[1], dim=-1).data.cpu().numpy()
         for i in range(14):
             idxs = []
@@ -196,27 +203,30 @@ def main():
             else:
                 drop = get_min_k(reduce_prob[i, :], num_to_drop[sp])
             for idx in drop:
-                switches_reduce[i][idxs[idx]] = False
+                switches_reduce[i][idxs[idx]] = False #注意此处更新了switches_reduce
         logging.info('switches_normal = %s', switches_normal)
         logging_switches(switches_normal)
         logging.info('switches_reduce = %s', switches_reduce)
         logging_switches(switches_reduce)
         
-        if sp == len(num_to_keep) - 1:
+        if sp == len(num_to_keep) - 1: #最后一个阶段
             arch_param = model.module.arch_parameters()
-            normal_prob = F.softmax(arch_param[0], dim=sm_dim).data.cpu().numpy()
+            normal_prob = F.softmax(arch_param[0], dim=sm_dim).data.cpu().numpy() #计算各个op概率
             reduce_prob = F.softmax(arch_param[1], dim=sm_dim).data.cpu().numpy()
-            normal_final = [0 for idx in range(14)]
+            normal_final = [0 for idx in range(14)] #记录每条path上选择的op的索引
             reduce_final = [0 for idx in range(14)]
             # remove all Zero operations
             for i in range(14):
-                if switches_normal_2[i][0] == True:
-                    normal_prob[i][0] = 0
-                normal_final[i] = max(normal_prob[i])
+                if switches_normal_2[i][0] == True: #如果Zero操作被选择了将其概率置为0，在最后一个阶段训练完成后，每条path还剩3个op
+                    normal_prob[i][0] = 0 #如果我们在第3阶段后，还有Zero operations，将其对应的概率置0
+                normal_final[i] = max(normal_prob[i]) #记录第i条path上选择的op的概率的最大值
+
                 if switches_reduce_2[i][0] == True:
                     reduce_prob[i][0] = 0
                 reduce_final[i] = max(reduce_prob[i])                
             # Generate Architecture, similar to DARTS
+            # 为每个计算节点选择两条输入path，由于0节点的输入是固定的，因此不用选
+            # 1Node候选path数3，2Node候选path数4，3Node候选path数5
             keep_normal = [0, 1]
             keep_reduce = [0, 1]
             n = 3
@@ -225,15 +235,17 @@ def main():
                 end = start + n
                 tbsn = normal_final[start:end]
                 tbsr = reduce_final[start:end]
-                edge_n = sorted(range(n), key=lambda x: tbsn[x])
+                edge_n = sorted(range(n), key=lambda x: tbsn[x]) #从候选的path中选择两条
                 keep_normal.append(edge_n[-1] + start)
                 keep_normal.append(edge_n[-2] + start)
+
                 edge_r = sorted(range(n), key=lambda x: tbsr[x])
                 keep_reduce.append(edge_r[-1] + start)
                 keep_reduce.append(edge_r[-2] + start)
                 start = end
                 n = n + 1
             # set switches according the ranking of arch parameters
+            # 设置switches，对于没有选择的path，将其上的op全部关掉
             for i in range(14):
                 if not i in keep_normal:
                     for j in range(len(PRIMITIVES)):
@@ -244,15 +256,16 @@ def main():
             # translate switches into genotype
             genotype = parse_network(switches_normal, switches_reduce)
             logging.info(genotype)
-            ## restrict skipconnect (normal cell only)
+            ## restrict skipconnect (normal cell only) 约束跳跃链接
             logging.info('Restricting skipconnect...')
             # generating genotypes with different numbers of skip-connect operations
+            # 生成不同数量skip-connect operations 的基因
             for sks in range(0, 9):
                 max_sk = 8 - sks                
                 num_sk = check_sk_number(switches_normal)               
                 if not num_sk > max_sk:
                     continue
-                while num_sk > max_sk:
+                while num_sk > max_sk: #删除多余的skip-connection
                     normal_prob = delete_min_sk_prob(switches_normal, switches_normal_2, normal_prob)
                     switches_normal = keep_1_on(switches_normal_2, normal_prob)
                     switches_normal = keep_2_branches(switches_normal, normal_prob)
@@ -271,7 +284,7 @@ def train(train_queue, valid_queue, model, network_params, criterion, optimizer,
         n = input.size(0)
         input = input.cuda()
         target = target.cuda(non_blocking=True)
-        if train_arch:
+        if train_arch: #训练网络结构参数
             # In the original implementation of DARTS, it is input_search, target_search = next(iter(valid_queue), which slows down
             # the training when using PyTorch 0.4 and above. 
             try:
@@ -281,14 +294,14 @@ def train(train_queue, valid_queue, model, network_params, criterion, optimizer,
                 input_search, target_search = next(valid_queue_iter)
             input_search = input_search.cuda()
             target_search = target_search.cuda(non_blocking=True)
-            optimizer_a.zero_grad()
+            optimizer_a.zero_grad() #更新结构参数
             logits = model(input_search)
             loss_a = criterion(logits, target_search)
             loss_a.backward()
             nn.utils.clip_grad_norm_(model.module.arch_parameters(), args.grad_clip)
             optimizer_a.step()
 
-        optimizer.zero_grad()
+        optimizer.zero_grad() # 训练网络权重
         logits = model(input)
         loss = criterion(logits, target)
 
@@ -344,7 +357,7 @@ def parse_network(switches_normal, switches_reduce):
             for j in range(start, end):
                 for k in range(len(switches[j])):
                     if switches[j][k]:
-                        gene.append((PRIMITIVES[k], j - start))
+                        gene.append((PRIMITIVES[k], j - start)) # op,path,记录第几条path上是什么Op
             start = end
             n = n + 1
         return gene
@@ -360,7 +373,7 @@ def parse_network(switches_normal, switches_reduce):
     
     return genotype
 
-def get_min_k(input_in, k):
+def get_min_k(input_in, k): #寻找input_in前k个最小值
     input = copy.deepcopy(input_in)
     index = []
     for i in range(k):
@@ -370,9 +383,9 @@ def get_min_k(input_in, k):
     
     return index
 def get_min_k_no_zero(w_in, idxs, k):
-    w = copy.deepcopy(w_in)
+    w = copy.deepcopy(w_in) #w_in 一条path上op的权重
     index = []
-    if 0 in idxs:
+    if 0 in idxs: #PRIMITIVES 中第1个op为none，即Zero Op
         zf = True 
     else:
         zf = False
@@ -399,21 +412,25 @@ def logging_switches(switches):
 def check_sk_number(switches):
     count = 0
     for i in range(len(switches)):
-        if switches[i][3]:
-            count = count + 1
+        if switches[i][3]: #第4个候选op是skip-connect
+            count = count + 1 #统计skip-connect的数量
     
     return count
 
 def delete_min_sk_prob(switches_in, switches_bk, probs_in):
+    # switches_in 每条path只有一个op
+    # switches_bk 每条path有3个op
+    # normal_prob = delete_min_sk_prob(switches_normal, switches_normal_2, normal_prob)
     def _get_sk_idx(switches_in, switches_bk, k):
-        if not switches_in[k][3]:
+        if not switches_in[k][3]: #如果最终第k条path上没有skip-connect
             idx = -1
-        else:
+        else: #如果第k条path上恰巧选择了skip-connect
             idx = 0
             for i in range(3):
-                if switches_bk[k][i]:
+                if switches_bk[k][i]: #switches_bk 目前只剩下3个op
                     idx = idx + 1
         return idx
+
     probs_out = copy.deepcopy(probs_in)
     sk_prob = [1.0 for i in range(len(switches_bk))]
     for i in range(len(switches_in)):
